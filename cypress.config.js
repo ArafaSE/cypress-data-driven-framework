a7073fe891a54eda2dd39a9e9534130eb80804c0
Mohamed Arafa <mohamed.arafa.swt@gmail.com>
2023-05-04 15:35:24 +0300

    Implement fillRequestForm command

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    retries:
+    {
+      runMode: 1,
+      openMode: 1,
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/order-request.cy.js
+++ cypress/e2e/order-request.cy.js
@@ -6,36 +6,63 @@ describe('Order Request Form', () => {
     })
   
     it('User can request order from google form', () => {
-      cy.get('[aria-labelledby="i11"]').type('Headphone').should('have.value', 'Headphone')
-    
-      cy.contains('I am a new customer').click({force:true});
-      cy.contains('color 1').click({force:true});
-
-      cy.get("[data-value='']").click({force:true});
-      cy.get("[data-value='4']").click({force:true});
       
-      cy.get("[data-value='4']").as('product')
-      cy.get('@product').click({force:true, multiple: true})
-      cy.get('@product').should('attr', 'aria-selected', "true");
+      cy.fillRequestForm(true, 223, [3, 4], 4, "Mohamed", "mohamed.arafa.swt@gmail.com", ["email", "Phone"], "Do you have a return policy?", "2023-05-30", "02:30" )
 
-      cy.get('[aria-labelledby="i36"]').as('inputName')
-      cy.get('@inputName').type('Mohamed').should('have.value', 'Mohamed')
-      cy.get('[aria-labelledby="i40"]').type('mohamed.arafa.swt@gmail.com').should('have.value', 'mohamed.arafa.swt@gmail.com')
+      cy.get("[role='heading']").should('have.text', 'Order Request')
+      cy.get(".c2gzEf > a").should('be.be.visible')
+    });
+});
 
-     
+Cypress.Commands.add('fillRequestForm', (isNewCustomer, productNum, colorsNumbers , productSize, name, email, preferredContacts, Questions, deliveryDate, deliveryTime ) => {
 
-      cy.contains('Phone').click({force: true})
-      cy.contains('Email').click({force: true})
+  isNewCustomer ? cy.contains('I am a new customer').click({force:true}) : 
+                  cy.contains('I am an existing customer').click({force:true})
 
-      cy.get('[aria-labelledby="i55"]').type('Do you have a return policy?')
+  cy.get('[aria-labelledby="i11"]')
+    .type(productNum)
+    .should('have.value', productNum)
 
-      cy.get("input[type='date']").as('inputDate')
-      cy.get('@inputDate').type("2023-05-30").should('have.value', '2023-05-30')
+  for (const color of colorsNumbers) {
+    cy.contains('color ' + color).click({force:true});
+  }
 
-      cy.get("[role='combobox']").first().type('04').should('have.value', '04')
-      cy.get("[role='combobox']").last().type('30').should('have.value', '30')
+  cy.get("div[role='option'][data-value='']").click({force: true})
+  cy.get("div[role='option'][data-value='" + productSize + "']").as('productSizeOption').click({force: true})
+  cy.get('@productSizeOption').click({force: true})
+  cy.get('@productSizeOption').should('attr', 'aria-selected', "true");
 
-      cy.get("[role='button']").eq(2).click()   
-    })
+  cy.get('[aria-labelledby="i36"]')
+    .type(name)
+    .should('have.value', name)
+
+  cy.get('[aria-labelledby="i40"]')
+    .type(email)
+    .should('have.value', email)
+
+  for (let contact of preferredContacts) {
+    contact = contact[0].toUpperCase() + contact.slice(1);
+    cy.contains(contact).click({force: true})
+  }
+
+  cy.get('[aria-labelledby="i55"]').type(Questions)
+
+  cy.get("input[type='date']")
+    .type(deliveryDate)
+    .should('have.value', deliveryDate)
+
+  const timeHour =  deliveryTime.split(":")[0]
+  const timeMinutes =  deliveryTime.split(":")[1]
+  cy.get("[role='combobox']")
+    .first()
+    .type(timeHour)
+    .should('have.value', timeHour)
+
+  cy.get("[role='combobox']")
+    .last()
+    .type(timeMinutes)
+    .should('have.value', timeMinutes)
+
+  cy.get("[role='button']").eq(2).click()
 
 });
