bf92ad875c8bc4ca6cd4d5a39f1f43c8f7bd29ba
Mohamed Arafa <mohamed.arafa.swt@gmail.com>
2023-05-05 10:28:07 +0300

    execlude uploading video when success

--- cypress.config.js
+++ cypress.config.js
@@ -10,6 +10,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'https://docs.google.com/forms/d/e/'
+    baseUrl: 'https://docs.google.com/forms/d/e/',
+    screenshotOnRunFailure: true,
+    video: true,
+    videoUploadOnPasses: false,
   },
 });
